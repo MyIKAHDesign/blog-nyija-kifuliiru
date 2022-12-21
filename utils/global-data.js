fc320a5bb97fc0e2a00f72bf2e1351d3bcad595e
MyIKAHDesign <99662370+MyIKAHDesign@users.noreply.github.com>
2022-12-21 23:13:06 +0200

    Nahindula ibindu biguma biguma bibere mu Kifuliiru

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Kifuliiru. Blog nyija Kifuliiru';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Blog nyija mu Kifuliiru';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'All rights reserved. Yakolwa na Ayivugwe Kabemba';
 
   return {
     name,
